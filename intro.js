6c460981118b1d4df02f35ed6d98daab3e942d93
Khurshedul51 <princtonkadus@gmail.com>
2024-12-11 22:36:30 +0600

    intro files added

---
+++ intro.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fullname = "Sakibul ALam";
+var age = 24;
+function print(fullname, age) {
+    console.log("name = ".concat(fullname, " age = ").concat(age));
+}
+print("Sakib", 24);
