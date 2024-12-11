18c7f9245388b6cc56a20e50c2ba006900fccbe3
Khurshedul51 <princtonkadus@gmail.com>
2024-12-11 23:09:39 +0600

    intro.ts updated

--- intro.ts
+++ intro.ts
@@ -2,7 +2,7 @@ let fullname: string = "Sakibul ALam"
 
 let age: number = 24
 
-function print(fullname: string, age: number){
+function print(fullname: string, age: number): void{
     console.log(`name = ${fullname} age = ${age}`)
 }
 
