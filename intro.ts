8ae44dd1e914fdfa5a9255a8a3a504c517808ea0
Khurshedul51 <princtonkadus@gmail.com>
2024-12-11 22:38:47 +0600

    intro.ts added

---
+++ intro.ts
@@ -0,0 +1,11 @@
+let fullname: string = "Sakibul ALam"
+
+let age: number = 24
+
+function print(fullname: string, age: number){
+    console.log(`name = ${fullname} age = ${age}`)
+}
+
+print("Sakib", 24)
+
+export {}
